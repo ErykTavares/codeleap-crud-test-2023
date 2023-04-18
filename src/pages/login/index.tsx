27dd8ab039f452d454e47091ef16fa721c53b5a3
ErykTavares <erictavares35@gmail.com>
2023-04-18 17:13:08 -0300

    feat: font adjustments

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,4 +1,13 @@
+import Footer from '@/components/footer';
 import React from 'react';
+import * as S from './style';
 
-const Login = (): JSX.Element => <div>login</div>;
+const Login = (): JSX.Element => (
+	<>
+		<S.ContainerStyle>
+			<div>login</div>
+		</S.ContainerStyle>
+		<Footer />
+	</>
+);
 export default Login;
---
+++ src/pages/login/style.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const ContainerStyle = styled.section`
+	height: calc(100vh - 60px);
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
