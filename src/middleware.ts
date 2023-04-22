2bdbc4b1343a1d8c5fe49ec305f52d3736163104
ErykTavares <erictavares35@gmail.com>
2023-04-21 23:20:06 -0300

    fix: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(req: NextRequest) {
-	const profile = JSON.parse(req.cookies.get('profile')?.value || '');
+	const profile = req.cookies.get('profile')?.value;
 
-	if (!profile?.userName) {
+	if (!profile) {
 		if (req.nextUrl.pathname.startsWith('/')) {
 			return NextResponse.redirect(new URL('/login', req.url));
 		}
